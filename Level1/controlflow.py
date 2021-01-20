d7b0fe452401ab447c5524265ca31ad6b358052a
ggichuru <ggichuru.dev@gmail.com>
2021-01-21 01:12:41 +0300

    If statements

---
+++ Level1/controlflow.py
@@ -0,0 +1,12 @@
+    ## IF STATEMENTS
+x = int(input("Please enter an integer: "))
+
+if x < 0:
+     x = 0
+     print('Negative changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
