065f5350009c74eb4ca7b78f2b206994aed586a3
G_Gichuru <ggichuru.dev@gmail.com>
2021-01-28 12:09:08 +0300

    quadratic equation

---
+++ Level1/safa.py
@@ -0,0 +1,29 @@
+import math
+
+def getX(a,b,c):
+    discriminant = b*b - 4*a*c
+    sqroot = math.sqrt(abs(discriminant))
+
+    if discriminant > 0:
+        print('The greatest root is: ')
+        x = (-b+sqroot)/(2*a)
+        y = (-b-sqroot)/(2*a)
+        maximum = max(x,y)
+        print(maximum)
+
+    elif discriminant == 0:
+        print(-b/(2*a))
+    else:
+        x1 = (-b/(2*a), ' + i', sqroot)
+        y1 = (-b/(2*a), ' - i', sqroot)
+        maxi = max(x1,y1)
+        print(maxi)
+
+a = -1
+b = 3
+c = 3
+
+if a == 0:
+    print('Incorrect equation')
+else:
+    getX(a,b,c)
